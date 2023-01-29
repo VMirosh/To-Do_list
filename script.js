5b6b95f1328467838961b99f72060669cc574337
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-01-29 16:20:40 +0100

    add refactor all file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Находим элементы на странице
+
 let createForm = document.querySelector('.form');
 let taskInput = document.querySelector('#task-input');
 let addButton = document.querySelector('#add-task-btn')
@@ -7,51 +7,46 @@ let templateTask = document.querySelector(".template-task");
 let emptyList = document.querySelector(".empty-list");
 
 
-// Добавляем новую задачу
+
 createForm.addEventListener('click', addTask);
 function addTask(event) {
   if (event.target.localName == "button") {
-   
-    if (!emptyList.hidden) {
-      emptyList.hidden = true;
-    }
+    if (!emptyList.hidden) emptyList.hidden = true;
+    
     let newTask = createTask(taskInput.value);
     showTask.prepend(newTask);
-    // Очистить окно
     taskInput.value = "";
     taskInput.focus();
 
   }
 }
-//Создаем новую задачу 
+
 function createTask(text) {
   let div = templateTask.content.cloneNode(true);
   let textValue = div.querySelector("#text-value");
   textValue.append(text);
   return div;
 }
- // Delete task
+
 showTask.addEventListener('click', deleteTask);
 function deleteTask(event) {
-  
-  if (event.target.dataset.action === 'delete') {
+  if (event.target.dataset.action !== 'delete') return;
 
-    let parenNode = event.target.closest('.task');
-    parenNode.remove();
-  }
-
+  let parenNode = event.target.closest('.task');
+  parenNode.remove();
   let tasksAll = document.querySelectorAll(".task");
   if (tasksAll.length === 0) {
     emptyList.hidden = false;
   }
 }
-// Done task
-showTask.addEventListener('click',doneTask);
-debugger
- function doneTask(event){
-  if(event.target.dataset.action === 'done'){
-    let parenNode = event.target.closest('.task');
-    let textTask = parenNode.querySelector('#text-value');
-    textTask.classList.toggle('done-task');
-  }
- }
+
+showTask.addEventListener('click', doneTask);
+
+function doneTask(event) {
+  if (event.target.dataset.action !== 'done') return;
+
+  let parenNode = event.target.closest('.task');
+  let textTask = parenNode.querySelector('#text-value');
+  textTask.classList.toggle('done-task');
+
+}
