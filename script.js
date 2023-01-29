e62e02de38d8f69d1fb10aec27e54e8a1fbfc807
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2023-01-29 13:47:57 +0100

    add clean input and refactoring

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ let addButton = document.querySelector('#add-task-btn')
 let showTask = document.querySelector(".shower-task");
 let templateTask = document.querySelector(".template-task");
 
-createForm.addEventListener('click',function(event){
-  if(event.target.localName == "button" ) {
-     debugger;
-        let newTask = createTask(taskInput.value);
-       showTask.append(newTask);
 
+createForm.addEventListener('click', addTask);
 
-    }
-});
- function createTask(text){
+function addTask(event) {
+  if (event.target.localName == "button") {
+    debugger;
+    let newTask = createTask(taskInput.value);
+    showTask.append(newTask);
+    // Очистить окно
+    taskInput.value = "";
+    taskInput.focus();
+  }
+}
+
+//Создаем новую задачу 
+function createTask(text) {
   let div = templateTask.content.cloneNode(true);
-   let textValue = div.querySelector("#text-value");
-
- textValue.append(text);
-
+  let textValue = div.querySelector("#text-value");
+  textValue.append(text);
   return div;
-
-  
+}
  
-
- }
